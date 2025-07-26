e8c57d3e44293d64cbea76b93c37652c53859cc5
Phuc Hung <nphung.htkhang2020@gmail.com>
2025-07-26 09:54:30 +0700

    Add edit password

--- src/main/kotlin/com/example/demo/service/UserService.kt
+++ src/main/kotlin/com/example/demo/service/UserService.kt
@@ -3,16 +3,18 @@ package com.example.demo.service
 import com.example.demo.dto.request.register
 import com.example.demo.model.UserModel
 import com.example.demo.repository.UserRepo
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 
 @Service
 class UserService(
-    private val userRepo: UserRepo
+    private val userRepo: UserRepo,
+    private val passwordEncoder: PasswordEncoder,
 ) {
     fun postUser(req: register) : UserModel {
         val userModel = UserModel(
-            password = req.password,
+            password = passwordEncoder.encode(req.password),
             email = req.email,
             phone = req.phone,
             role = "USER",
